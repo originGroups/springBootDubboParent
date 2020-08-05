8368c54fa733647ec4b9f7e4a8b6fb733d484141
yuanqiang <12345678>
2020-08-05 11:44:11 +0800

    添加日志

--- userServiceProvider/src/main/java/com/UserServiceProviderApplication.java
+++ userServiceProvider/src/main/java/com/UserServiceProviderApplication.java
@@ -1,8 +1,10 @@
 package com;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo  //开启dubbo
 @SpringBootApplication
 public class UserServiceProviderApplication {
 
--- userServiceProvider/src/main/java/com/service/impl/UserServiceImpl.java
+++ userServiceProvider/src/main/java/com/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.service.impl;
 
-
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.bean.UserAddress;
 import com.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -11,7 +13,7 @@ import java.util.List;
 
 /**
  * 1.将服务提供者注册到注册中心（暴露服务）
- *      a.导入dubbo依赖
+ *      a.导入sringboot-dubbo依赖及其他dubbo依赖
  *      b.配置服务提供者
  * 2.让服务消费者到注册中心订阅服务提供者的服务地址
  *
@@ -19,9 +21,27 @@ import java.util.List;
 @Service   //应该引入dubbo的service
 @Component //springboot 组件 因为要引入dubbo的service注解，所以spring的service不能用了，只能引入Component
 public class UserServiceImpl implements UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     public List<UserAddress> qryUserAddressList(String userId) {
-        UserAddress userAddress1 = new UserAddress(1, "北京大兴区经开大厦", "10001", "Tom", "18402021042", "N");
-        UserAddress userAddress2 = new UserAddress(2, "北京大兴区大雄郁金香", "10001", "Tom", "18402021042", "Y");
+        logger.info("Begin call UserServiceImpl qryUserAddressList method  and request params{} " , JSON.toJSONString(userId));
+        UserAddress userAddress1 = new UserAddress(
+                1,
+                "北京大兴区经开大厦",
+                "10001",
+                "Tom",
+                "18402021042",
+                "N"
+        );
+        UserAddress userAddress2 = new UserAddress(
+                2,
+                "北京大兴区大雄郁金香",
+                "10001",
+                "Tom",
+                "18402021042",
+                "Y"
+        );
         List<UserAddress> userAddresses = Arrays.asList(userAddress1, userAddress2);
         System.out.println(userAddresses);
         return userAddresses;
